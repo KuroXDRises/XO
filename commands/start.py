dfc34cbefc2237fd9afa46c91817d3586cbec676
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2026-04-04 12:13:50 +0530

    Create start.py

---
+++ commands/start.py
@@ -0,0 +1,11 @@
+from db import *
+from bot import bot
+from telebot.types import (
+InlineKeyboardMarkup,
+InlineKeyboardButton
+)
+pic = "https://i.ibb.co/Gv794MpQ/8aba2109900b.jpg"
+@bot.message_handler(commands=["start"])
+async def start_command(message):
+    message.from_user.id
+    user = check_user(user_id)
