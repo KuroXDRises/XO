7a9322cdf9b25c578e50b5115c0cb63b13a4e9f0
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2026-04-04 21:38:07 +0530

    Update lb.py

--- commands/lb.py
+++ commands/lb.py
@@ -4,3 +4,17 @@ from db import *
 def get_lb_users():
     db = load_db()
     users = []
+    for uid, data in db.items():
+        users.append(
+                  "_id": uid,
+                  "name": data['name'],
+                  "level": data['level'],
+                  "exp": data['exp']}
+        )
+    users = sorted(
+        users,
+        key=lambda x:
+        (x['level'],
+        x['exp']),
+        reverse=True
+    )
