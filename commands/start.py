7dce45e241e9696da0ce49b743a8bbb8eab2035c
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2026-04-04 12:20:14 +0530

    Update start.py

--- commands/start.py
+++ commands/start.py
@@ -8,4 +8,4 @@ pic = "https://i.ibb.co/Gv794MpQ/8aba2109900b.jpg"
 @bot.message_handler(commands=["start"])
 async def start_command(message):
     message.from_user.id
-    user = check_user(user_id)
+    user = get_user(user_id)
