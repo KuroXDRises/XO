fe8b361005eecbb6166195ff298a87ddc204ae1d
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2026-04-04 21:47:56 +0530

    Update lb.py

--- commands/lb.py
+++ commands/lb.py
@@ -6,7 +6,7 @@ def get_lb_users():
     users = []
     for uid, data in db.items():
         users.append(
-                  "_id": uid,
+                  "uid": uid,
                   "name": data['name'],
                   "level": data['level'],
                   "exp": data['exp']}
@@ -18,3 +18,15 @@ def get_lb_users():
         x['exp']),
         reverse=True
     )
+@bot.message_handler(commands=["leaderboard"])
+async def leaderboard_handler(message):
+    top10 = get_kb_users()[:10]
+    text = f"[𝗫𝗢] 𝗧𝗢𝗣 𝗣𝗟𝗔𝗬𝗘𝗥𝗦 [𝗫𝗢]\n\n"
+    rank = 1
+    for top in top10:
+        text+=f"<i>{rank}• {top["name"]} | {top["level"]} | {top["exp"]}</i>"
+    await bot.reply_to(
+        message,
+        text
+    )
+        
