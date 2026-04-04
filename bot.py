22c9e43e41f1dcb93d4b87d9f3e1eb4f36fbf77e
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2026-04-04 13:49:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 from telebot.async_telebot import AsyncTeleBot as atb
 from Config import config
-class main(atb):
-    def __init__(self):
-        super().__init__(
-            config.TOKEN,
-            parse_mode="Markdown"
-        )
-bot = main
+
+bot = atb(
+    config.TOKEN,
+    parse_mode="Markdown"
+)
